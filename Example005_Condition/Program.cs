09e67b9700f63b9246047a6c710d138cb6236969
rvkstudent <rvkstudent@yandex.ru>
2023-02-11 09:46:30 +0300

    Example 5

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("Hi, Masha");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
