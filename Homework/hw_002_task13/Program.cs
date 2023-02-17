63f05b505eea16f179f51bb50012d45ca7ba01dc
rvkstudent <rvkstudent@yandex.ru>
2023-02-17 14:49:12 +0300

    dz 2 done

---
+++ Homework/hw_002_task13/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+System.Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(System.Console.ReadLine());
+int temp_num = num1 / 100;
+System.Console.WriteLine($"{temp_num}");
+if (temp_num > 10)
+{
+    while (temp_num / 10 > 100)
+    {
+        temp_num = temp_num / 10;       
+    }
+
+    System.Console.WriteLine($"Третья цифра в числе: {temp_num % 10}");
+
+}
+else if (temp_num > 0)
+{
+    System.Console.WriteLine($"Третья цифра в числе: {num1 % 10}");
+}
+else
+{
+    System.Console.WriteLine("В этом числе нет третьей цифры.");
+}
---
+++ Homework/hw_002_task15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+System.Console.Write("Введите число: ");
+int user_number = Convert.ToInt32(System.Console.ReadLine());
+
+if (user_number > 5 && user_number <=7 )
+{
+    System.Console.WriteLine("Да - этот день является выходным.");
+}
+else if (user_number > 0 && user_number <=5)
+{
+    System.Console.WriteLine("Нет - этот день не является выходным.");
+}
+else
+{
+    System.Console.WriteLine("Введенное число не соответствует дню недели.");
+}
