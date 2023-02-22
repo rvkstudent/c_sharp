2afe3c4f8a982c794a0a6085ba4942eee34a5c85
rvkstudent <rvkstudent@yandex.ru>
2023-02-22 13:33:46 +0300

    homework3

---
+++ Seminars/Seminars3/task24/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int A)
+{
+    int sum = 0;
+    for (int i = 0; i <= A; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+
+int number = ReadInt("Введите число A: ");
+System.Console.WriteLine($"Сумма чисел от 1 до {number} равна {Sum(number)}");
+
+number = ReadInt("Введите число B: ");
+System.Console.WriteLine($"Сумма чисел от 1 до {number} равна {Sum(number)}");
+
+number = ReadInt("Введите число C: ");
+System.Console.WriteLine($"Сумма чисел от 1 до {number} равна {Sum(number)}");
