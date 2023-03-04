c5fc3fc4171c06250bc6eef3e4e2d3ec135ce5b5
rvkstudent <rvkstudent@yandex.ru>
2023-03-04 11:07:33 +0300

    homework 6

---
+++ Homework/hw006_task41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Урок 6. Одномерные массивы. Продолжение
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] GetNumbersFromUser()
+{
+    System.Console.Write("Введите числа через пробел: ");
+    return Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+}
+
+string PrintArray(int[] array)
+{
+    return String.Join(" ", array);
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) counter++;
+    }
+    return counter;
+}
+
+System.Console.WriteLine($"Количество положительных чисел равно {CountPositiveNumbers(GetNumbersFromUser())}");
---
+++ Homework/hw006_task43/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] GetNumbersFromUser()
+{    
+    return Array.ConvertAll(Console.ReadLine().Split(","), double.Parse);
+}
+
+double[] GetIntersectionPoint(double [] coord1, double [] coord2)
+{
+    double X = (coord2[0] - coord1[0]) / (coord1[1] - coord2[1]);
+    System.Console.WriteLine(X);
+    return new double []{ X, 
+    coord1[1] * X + coord1[0]};
+}
+
+System.Console.Write("Введите b1 и k1 через запятую: ");
+double [] coord1 = GetNumbersFromUser();
+
+System.Console.Write("Введите b2 и k2 через запятую: ");
+double [] coord2 = GetNumbersFromUser();
+System.Console.WriteLine($"Точка пересечения прямых: ({String.Join(" ", GetIntersectionPoint(coord1, coord2))})");
+
